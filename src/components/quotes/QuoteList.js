0a0e5bc0b1d04c86cb3d558ebfa2afa554da1cc8
PatrickPei747 <754463323@qq.com>
2022-12-27 17:39:35 +1300

    Send and output via http

--- src/components/quotes/QuoteList.js
+++ src/components/quotes/QuoteList.js
@@ -1,14 +1,41 @@
 import { Fragment } from 'react';
+import { useHistory, useLocation } from 'react-router-dom';
 
 import QuoteItem from './QuoteItem';
 import styles from './QuoteList.module.css';
 
+const sortQuotes = (quotes, ascending) => {
+  return quotes.sort((quoteA, quoteB) => {
+    if (ascending) {
+      return quoteA.id > quoteB.id ? 1 : -1;
+    } else {
+      return quoteA.id < quoteB.id ? 1 : -1;
+    }
+  });
+};
+
 const QuoteList = (props) => {
+  const history = useHistory();
+  const location = useLocation();
+
+  const queryParams = new URLSearchParams(location.search);
+  const isSortAscending = queryParams.get('sort') === 'asc';
+
+  const sortedQuote = sortQuotes(props.quotes, isSortAscending);
+
+  const changeSortingHandler = () => {
+    history.push(`${location.pathname}?sort=${(isSortAscending ? 'dec' : 'asc')}`);
+  };
+
   return (
     <Fragment>
-      <div className={styles.sorting}></div>
+      <div className={styles.sorting}>
+        <button onClick={changeSortingHandler}>
+          Sort {isSortAscending ? 'Descending' : 'Ascending'}
+        </button>
+      </div>
       <ul className={styles.list}>
-        {props.quotes.map((quote) => (
+        {sortedQuote.map((quote) => (
           <QuoteItem
             key={quote.id}
             id={quote.id}
--- src/pages/QuoteDetail.js
+++ src/pages/QuoteDetail.js
@@ -1,27 +1,51 @@
-import { Fragment } from "react";
-import { useParams, Route } from "react-router-dom";
+import { Fragment, useEffect } from "react";
+import { useParams, Route, Link, useRouteMatch } from "react-router-dom";
 
 import HighlightedQuote from '../components/quotes/HighlightedQuote';
 import Comments from '../components/comments/Comments'
-
-const DUMMY_QUOTES = [
-    {id: 'q1', author: 'Patrick', text: 'Today is Boxing Day!'},
-    {id: 'q2', author: 'Haopu', text: 'Chicken livers are delicious!'}
-];
+import LoadingSpinner from "../components/UI/LoadingSpinner";
+import useHttp from '../hooks/use-http';
+import { getSingleQuote } from '../lib/api';
 
 const QuoteDetail = () => {
     const params = useParams();
+    const match = useRouteMatch();
 
-    const quote = DUMMY_QUOTES.find(quote => quote.id === params.quoteId);
+    const { quoteId } = params;
 
-    if(!quote) {
+    const { sendRequest, status, data: loadedQuote, error } = useHttp(getSingleQuote, true);
+
+    useEffect(() => {
+        sendRequest(quoteId)
+    }, [sendRequest, quoteId]);
+
+    if (status === 'pending') {
+        return (
+            <div className='centered'>
+                <LoadingSpinner />
+            </div>
+        );
+    }
+
+    if (error) {
+        return (
+            <p className='centered focused'>{error}</p>
+        );
+    }
+
+    if (!loadedQuote.text) {
         return <p>No Quote Found!</p>
     }
 
     return (
         <Fragment>
-            <HighlightedQuote text={quote.text} author={quote.author} />
-            <Route path={`/quotes/${params.quoteId}/comments`}>
+            <HighlightedQuote text={loadedQuote.text} author={loadedQuote.author} />
+            <Route path={match.path} exact>
+                <div className='centered'>
+                    <Link className='btn--flat' to={`${match.url}/comments`}>Load Comments</Link>
+                </div>
+            </Route>
+            <Route path={`${match.path}/comments`}>
                 <Comments />
             </Route>
         </Fragment>
